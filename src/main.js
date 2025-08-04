0a8dfdea15a949d068c1899c5f1d55df34859470
Moxvallix Mox <moxvallix@gmail.com>
2025-08-04 15:27:37 +0930

    chore: export NCRSBanner

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
+import { NCRSBanner } from "./data/banner";
 import NCRSBannerUI from "./ui";
 import NCRSBannerPreview from "./components/banner_preview";
 import NCRSBannerPatternPreview from "./components/pattern_preview";
 import NCRSCommandBox from "./components/command_box";
 import NCRSBannerInstructions from "./components/instructions";
 
-export { NCRSBannerUI, NCRSBannerPreview, NCRSBannerPatternPreview, NCRSCommandBox, NCRSBannerInstructions };
+export { NCRSBanner, NCRSBannerUI, NCRSBannerPreview, NCRSBannerPatternPreview, NCRSCommandBox, NCRSBannerInstructions };
